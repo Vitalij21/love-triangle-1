02e51181ffdd77f00580f1cd2c0106f28e9ee1d3
Vitalij21 <41307680+Vitalij21@users.noreply.github.com>
2019-02-28 00:37:09 +0300

    love-triangle-1-my

--- src/index.js
+++ src/index.js
@@ -4,4 +4,36 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var a, b, c, d;
+  var m=0;
+  //b=preferences[a];
+  //c=preferences[b];
+  for (var i=0; i<preferences.length; i++) {
+
+    if (preferences [i]!=0){
+    var f = preferences[i];
+    //var p = preferences[i-1];
+    var p = preferences[f-1];
+      
+    if (f != p) {
+
+      if (i==preferences[p-1]-1) {
+
+        preferences[i]=0;
+        preferences[f-1]=0;
+        preferences[p-1]=0;
+
+        m++;
+      }
+    }
+
+    }
+  //a=i;
+  //b=preferences[a];
+  //c=preferences[b];
+  //if(i==preferences[preferences[preferences[i]]]) {
+  //d++;
+  //};
+  };
+  return m;
+  };
